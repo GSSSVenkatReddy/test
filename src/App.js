1aa7aa0584ea78a7023b916ec6a1ea3e04d9b9e7
GSSSVenkatReddy <venkatreddygudla@gmail.com>
2023-03-31 19:24:24 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90
+};
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TestHeading heading={{text: 'test', size: 1}} />
+      
+      <MyButton />
+
+      <TestImage />
+
+      <ShoppingList />
+      
+      <TestHeading heading={{text: 'Counters that update seperately', size: 2}} />
+      <ButtonWithState /><br />
+      <ButtonWithState />
     </div>
+  );
+}
+
+function TestHeading({heading}) {
+  if(heading.size === 1) {
+    return(
+      <h1>{heading.text}</h1>
+    );
+  }
+  if(heading.size === 2) {
+    return(
+      <h2>{heading.text}</h2>
+    );
+  }
+}
+
+function TestImage() {
+  return(
+    <div>
+      <h2>{user.name}</h2>
+
+      <img 
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </div>
+  );
+}
+
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>I'm a button</button>
+  );
+}
+
+function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
+  return(
+    <ul>{listItems}</ul>
+  );
+}
+
+function ButtonWithState() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return(
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
   );
 }
 
